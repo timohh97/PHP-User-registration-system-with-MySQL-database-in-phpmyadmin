a49cd5ca05117f98bb983a7d511f6a233f3c9e31
Timo <timo.schessl@gmx.de>
2020-04-26 17:50:21 +0200

    Inserting into database works

--- register.php
+++ register.php
@@ -7,13 +7,23 @@ echo "Connection successful.";
 
 if($_SERVER['REQUEST_METHOD']=='POST')
 {
-  if($_POST['pw']==$_POST['pw2'])
+  if($_POST['password1']==$_POST['password2'])
   {
     echo "passwords are the same!";
 
-    $username = $mysqli->real_escape_string($_POST['username']);
+    $username = $_POST['username'];
+    $password = $_POST['password1'];
+    $id = rand();
 
-    echo $username;
+
+    $query = "INSERT INTO user (id,username,password) VALUES ('$id','$username','$password')";
+
+    echo $database->query($query);
+
+
+  }
+  else {
+    echo "Passwords are not equal!";
   }
 }
 
